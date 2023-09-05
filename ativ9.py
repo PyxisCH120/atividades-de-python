49f007d6fa7c53a150d715f8d459812b6ad46f56
Mateus Micael <143049393+PyxisCH120@users.noreply.github.com>
2023-09-05 18:49:41 -0300

    Create ativ9.py

---
+++ ativ9.py
@@ -0,0 +1,20 @@
+vetorA = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+
+lista = list(vetorA)
+
+num1 = lista[0]
+num2 = lista[1]
+num3 = lista[2]
+num4 = lista[3]
+num5 = lista[4]
+num6 = lista[5]
+num7 = lista[6]
+num8 = lista[7]
+num9 = lista[8]
+num10 = lista[9]
+
+
+soma_quadrados = num1**2 + num2**2 + num3**2 + num4**2 + num5**2 + num6**2 + num7**2 + num8**2 + num9**2 + num10**2
+
+print("A soma dos quadrados dos números é:", soma_quadrados)
