8388d60502be943c29984facc2ad3361104989a1
Mateus Micael <143049393+PyxisCH120@users.noreply.github.com>
2023-09-05 18:49:03 -0300

    Create ativ2.py

---
+++ ativ2.py
@@ -0,0 +1,14 @@
+MeuVetor = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+MeuVetorLista = list(MeuVetor)
+
+print(MeuVetorLista[9])
+print(MeuVetorLista[8])
+print(MeuVetorLista[7])
+print(MeuVetorLista[6])
+print(MeuVetorLista[5])
+print(MeuVetorLista[4])
+print(MeuVetorLista[3])
+print(MeuVetorLista[2])
+print(MeuVetorLista[1])
+print(MeuVetorLista[0])
