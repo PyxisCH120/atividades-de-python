5045920edd3d37c14298227fbcf8b464cd0a6781
Mateus Micael <143049393+PyxisCH120@users.noreply.github.com>
2023-09-05 18:49:24 -0300

    Create ativ3.py

---
+++ ativ3.py
@@ -0,0 +1,18 @@
+MeuVetor = {1, 2, 3, 4}
+
+MeuVetorLista = list(MeuVetor)
+
+num1 = MeuVetorLista[0]
+num2 = MeuVetorLista[1]
+num3 = MeuVetorLista[2]
+num4 = MeuVetorLista[3]
+
+media = (num1 + num2 + num3 + num4) / 4
+
+print(media)
+
+
+
+
+
+
